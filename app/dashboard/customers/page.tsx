05075e3ad99d80096a44f10cc0e435a4699b8ee2
Dylan Girrens <dgirrens1231@yahoo.com>
2025-06-11 00:56:17 -0400

    Suspense

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,8 @@
-export default function Page() {
+export default async function Page() {
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    console.log('Data fetch completed after 3 seconds.');
     return (
         <p>Customers Page</p>
     )
