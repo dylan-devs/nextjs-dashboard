2ffb41d07410f222fef818f0986280515d7a3477
Dylan Girrens <dgirrens1231@yahoo.com>
2025-06-19 13:37:22 -0400

    removed timeout

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,8 +1,4 @@
 export default async function Page() {
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    console.log('Data fetch completed after 3 seconds.');
     return (
         <p>Customers Page</p>
     )
