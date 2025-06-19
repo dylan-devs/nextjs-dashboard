9cfb000cb2a9aa9d3cd927d2e67f6958fa5221d2
Dylan Girrens <dgirrens1231@yahoo.com>
2025-06-19 12:10:54 -0400

    chapter 12 complete

---
+++ app/dashboard/customers/loading.tsx
@@ -0,0 +1,7 @@
+import { lusitana } from '@/app/ui/fonts';
+
+export default function Loading() {
+    return (
+        <h1 className={`${lusitana.className} font-bold text-purple-800 text-2xl`}>Loading...</h1>
+    );
+}
