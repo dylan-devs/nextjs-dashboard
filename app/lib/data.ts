715c25372ed078286659c235c6c88d0a190e23c7
Dylan Girrens <dgirrens1231@yahoo.com>
2025-06-19 13:33:02 -0400

    paid problem

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -73,6 +73,8 @@ export async function fetchCardData() {
     const totalPaidInvoices = formatCurrency(data[2][0].paid ?? '0');
     const totalPendingInvoices = formatCurrency(data[2][0].pending ?? '0');
 
+    console.log(totalPaidInvoices);
+
     return {
       numberOfCustomers,
       numberOfInvoices,
